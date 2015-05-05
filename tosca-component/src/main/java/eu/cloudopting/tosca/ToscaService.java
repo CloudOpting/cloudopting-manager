2a2f851cf6f65379164eaeb4c38628b880304736
Luca Gioppo <gioppoluca@libero.it>
2015-05-05 12:30:45 +0200

    First integratoin of modules not working

--- tosca-component/src/main/java/eu/cloudopting/tosca/ToscaService.java
+++ tosca-component/src/main/java/eu/cloudopting/tosca/ToscaService.java
@@ -1,5 +1,13 @@
 package eu.cloudopting.tosca;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ToscaService {
 
+	byte[] getToscaGraph( String id){
+		return null;
+		
+	}
 }
