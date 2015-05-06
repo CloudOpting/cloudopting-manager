58cdc1ec1ca1f4ac9703e251e4d55959515a4bb8
Luca Gioppo <gioppoluca@libero.it>
2015-05-06 11:21:44 +0200

    Added some starting code for bpmn

---
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/BpmnService.java
@@ -0,0 +1,32 @@
+package eu.cloudopting.bpmn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BpmnService {
+	private final Logger log = LoggerFactory.getLogger(BpmnService.class);
+	
+	public String startDeployProcess(){
+		return null;
+	
+	}
+	
+	public String startTestDeployProcess(){
+		return null;
+	
+	}
+	
+	public String startAddApplicationProcess(){
+		return null;
+	
+	}
+	
+	public byte[] getProcessStatusImage(String id){
+		return null;
+	
+	}
+
+}
---
+++ rest-component/src/main/java/eu/cloudopting/web/rest/BpmnController.java
@@ -0,0 +1,51 @@
+package eu.cloudopting.web.rest;
+
+import javax.annotation.security.RolesAllowed;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import eu.cloudopting.bpmn.BpmnService;
+import eu.cloudopting.security.AuthoritiesConstants;
+import eu.cloudopting.tosca.ToscaService;
+
+/**
+ * REST controller for managing tosca
+ */
+@RestController
+@RequestMapping("/api")
+public class BpmnController {
+	private final Logger log = LoggerFactory.getLogger(BpmnController.class);
+
+	@Autowired
+	private BpmnService bpmn;
+	
+    @RequestMapping(value = "/bpmn/imageStatus/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE)
+    @RolesAllowed(AuthoritiesConstants.ANONYMOUS)
+    @ResponseBody byte[] getToscaGraph(@PathVariable String id) {
+        log.info("REST request to get graph of tosca id : {}", id);
+        return bpmn.getProcessStatusImage(id);
+    }
+    
+	@RequestMapping(value = "/process", method = RequestMethod.POST, headers = "content-type=application/x-www-form-urlencoded")
+	public @ResponseBody String startProcessInstance(
+			@RequestParam(value = "customerId", required = false) String customerId,
+			@RequestParam(value = "cloudId", required = false) String cloudId,
+			@RequestParam(value = "toscaId", required = false) String toscaId) {
+		String pid = bpmn.startDeployProcess();
+		System.out.println("returning pid: " + pid);
+		return pid;
+	}
+
+}
