154de7facbb742607b864f9cb8ffdb6886a24848
Luca Gioppo <gioppoluca@libero.it>
2015-05-11 09:48:18 +0200

    First connection of the BPMN to the DB

--- bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployManageCustomization.java
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployManageCustomization.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import eu.cloudopting.domain.Customizations;
+import eu.cloudopting.service.CustomizationService;
 import eu.cloudopting.tosca.ToscaService;
 
 @Service
@@ -14,11 +16,17 @@ public class DeployManageCustomization implements JavaDelegate {
 	private final Logger log = LoggerFactory.getLogger(DeployManageCustomization.class);
 	@Autowired
 	ToscaService toscaService;
+	
+	@Autowired
+	CustomizationService cusomizationS;
 
 	@Override
-	public void execute(DelegateExecution arg0) throws Exception {
+	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
 		log.debug("in Deploy");
+		String customizationId = (String) execution.getVariable("customizationId");
+		Customizations theCust = cusomizationS.findOne(Long.parseLong(customizationId));
+		log.info(theCust.toString());
 		toscaService.getNodeType("");
 		
 	}
