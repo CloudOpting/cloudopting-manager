9830bd534614b28a8d84bdb299612226bf446d7c
j-fuentes <jotaefe555555@gmail.com>
2015-05-13 00:55:43 +0200

    Added skell for methods and classes.

---
+++ docker-component/src/main/java/eu/cloudopting/docker/DockerError.java
@@ -0,0 +1,19 @@
+package eu.cloudopting.docker;
+
+
+/**
+ * 
+ * TODO: javadoc
+ *
+ */
+public class DockerError extends Exception{
+    public DockerError() {}
+
+    public DockerError(String message){
+       super(message);
+    }
+
+}
+
+
+
--- docker-component/src/main/java/eu/cloudopting/docker/DockerService.java
+++ docker-component/src/main/java/eu/cloudopting/docker/DockerService.java
@@ -4,32 +4,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import eu.cloudopting.docker.CraneRestClient;
+import eu.cloudopting.docker.DockerBuilder;
+import eu.cloudopting.docker.DockerCluster;
+import eu.cloudopting.docker.DockerComposer;
 
+/**
+ * 
+ * TODO: javadoc
+ *
+ */
 @Service
 public class DockerService {
 	private final Logger log = LoggerFactory.getLogger(DockerService.class);
-	
-	public String buildDockerImage(String customer, String service, String dockerfile,String path){
-		// TODO imagename is dummy API will return the imagename so that in sequent calls we can do check on asynch work
-		String imagename=null;
-		return imagename;
-		
-	}
-	
-	public boolean isImageBuilt(String imagename){
-		return false;
-	}
-	
-	public String runDockerCompose(String customer, String service, String path){
-		// TODO we need to define what we return from docker-compose asynch execution
-		// imagename is no more consistent
-		String imagename=null;
-		return imagename;
 
-		
+	public CraneRestClient restClient;
+	public DockerBuilder builder;
+	public DockerCluster cluster;
+	public DockerComposer composer;
+	
+	public DockerService (){
+		this("http://localhost:8888");	// By default the cloudopting-crane api end-point will be port 8888 on localhost.
 	}
 	
-	public boolean isDockerComposeRunning(String token){
-		return false;
+	public DockerService(String uri){
+		this.restClient = new CraneRestClient(uri);
+		this.builder = new DockerBuilder(restClient);
+		this.cluster = new DockerCluster(restClient);
+		this.composer = new DockerComposer(restClient);
 	}
+
 }
+
+
+
