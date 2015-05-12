61c077c2e9ee073d98e3c9ac8e5e621386645fe0
Daniel P <daniel.posirca@teamnet.ro>
2015-05-12 14:56:10 +0300

    jackrabbit implementation and mimetype utils

---
+++ common-component/src/main/java/eu/cloudopting/exception/CommonException.java
@@ -0,0 +1,26 @@
+package eu.cloudopting.exception;
+
+/**
+ * Exception thrown by util module.
+ */
+public class CommonException extends RuntimeException {
+    public CommonException() {
+        super();
+    }
+
+    public CommonException(String message) {
+        super(message);
+    }
+
+    public CommonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommonException(Throwable cause) {
+        super(cause);
+    }
+
+    protected CommonException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ common-component/src/main/java/eu/cloudopting/util/StreamUtil.java
@@ -0,0 +1,53 @@
+package eu.cloudopting.util;
+
+import eu.cloudopting.exception.CommonException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Utility class to work with streams
+ */
+public class StreamUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(StreamUtil.class);
+
+    private StreamUtil() {
+        throw new ExceptionInInitializerError("This is a utility class");
+    }
+
+    public static byte[] getBytesFromInputStream(InputStream inStream) {
+        // Get the size of the file
+        long streamLength = 0;
+        byte[] bytes = new byte[0];
+        try {
+            streamLength = inStream.available();
+
+            if (streamLength > Integer.MAX_VALUE) {
+                throw new CommonException("File too big!!");
+            }
+            // Create the byte array to hold the data
+            bytes = new byte[(int) streamLength];
+            // Read in the bytes
+            int offset = 0;
+            int numRead = 0;
+            while (offset < bytes.length
+                    && (numRead = inStream.read(bytes,
+                    offset, bytes.length - offset)) >= 0) {
+                offset += numRead;
+            }
+            // Ensure all the bytes have been read in
+            if (offset < bytes.length) {
+                throw new IOException("Could not completely read file ");
+            }
+            // Close the input stream and return bytes
+            inStream.close();
+        } catch (IOException e) {
+            log.error("An error occurred",e);
+            throw new CommonException(e);
+        }
+        return bytes;
+    }
+}
