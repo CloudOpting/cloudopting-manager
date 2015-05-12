a47c19c04e4e225bfd9a36eec6042d2e95bb45e3
Luca Gioppo <gioppoluca@libero.it>
2015-05-12 16:06:30 +0200

    Added CloudService and linked to bpmn + tosca cloud data

--- bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployCheckVm.java
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployCheckVm.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import eu.cloudopting.cloud.CloudService;
 import eu.cloudopting.tosca.ToscaService;
 
 @Service
@@ -14,14 +15,21 @@ public class DeployCheckVm implements JavaDelegate {
 	private final Logger log = LoggerFactory.getLogger(DeployCheckVm.class);
 	@Autowired
 	ToscaService toscaService;
+	
+	@Autowired
+	CloudService cloudService;
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
 		log.debug("in DeployCheckVm");
 		String customizationId = (String) execution.getVariable("customizationId");
+		String cloudtask = (String) execution.getVariable("cloudtask");
+		String cloudId = (String) execution.getVariable("cloudId");
+		
 		toscaService.getNodeType(customizationId,"");
-		execution.setVariable("vmInstalled", true);
+		boolean check = cloudService.checkVM(cloudId, cloudtask);
+		execution.setVariable("vmInstalled", check);
 		
 	}
 
--- bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployGenerateVm.java
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployGenerateVm.java
@@ -1,5 +1,7 @@
 package eu.cloudopting.bpmn.tasks.deploy;
 
+import java.util.HashMap;
+
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import eu.cloudopting.cloud.CloudService;
 import eu.cloudopting.tosca.ToscaService;
 
 @Service
@@ -14,12 +17,20 @@ public class DeployGenerateVm implements JavaDelegate {
 	private final Logger log = LoggerFactory.getLogger(DeployGenerateVm.class);
 	@Autowired
 	ToscaService toscaService;
+	
+	@Autowired
+	CloudService cloudService;
 
 	@Override
-	public void execute(DelegateExecution arg0) throws Exception {
+	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
 		log.debug("in DeployGenerateVm");
-//		toscaService.getNodeType("");
+		String customizationId = (String) execution.getVariable("customizationId");
+		String cloudId = (String) execution.getVariable("cloudId");
+		HashMap<String, String> data = toscaService.getCloudData(customizationId);
+		String cloudtask = cloudService.createVM(cloudId, data.get("cpu"), data.get("memory"), data.get("disk"));
+		execution.setVariable("cloudtask", cloudtask);
+		
 		
 	}
 
