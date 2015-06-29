154236d072056e1c80ba702dde6d352378c92052
Xavier Cases <xavier.cases.external@atos.net>
2015-06-29 14:29:44 +0200

    Added 'subscriber' screen. Changed styles to use it in other screens.

---
+++ rest-component/src/main/webapp/scripts/app/service/subscriber/subscriber.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cloudoptingApp')
+    .controller('SubscriberController', function ($scope) {
+
+        $scope.app = {
+            "name" : "Clear&ograve;"
+        };
+    }
+);
+
---
+++ rest-component/src/main/webapp/scripts/app/service/subscriber/subscriber.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('cloudoptingApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('subscriber', {
+                parent: 'service',
+                url: '/subscriber',
+                data: {
+                    roles: ['ROLE_ADMIN']
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/service/subscriber/subscriber.html',
+                        controller: 'SubscriberController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('subscriber');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
