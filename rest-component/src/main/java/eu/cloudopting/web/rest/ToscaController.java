42b4f0f538de14da9a3182249fbc47cdac296748
Luca Gioppo <gioppoluca@libero.it>
2015-07-20 11:14:17 +0200

    deeper logging for nailing crane problems and new API for dyn forms

--- rest-component/src/main/java/eu/cloudopting/web/rest/ToscaController.java
+++ rest-component/src/main/java/eu/cloudopting/web/rest/ToscaController.java
@@ -2,6 +2,9 @@ package eu.cloudopting.web.rest;
 
 import javax.annotation.security.RolesAllowed;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +37,37 @@ public class ToscaController {
     @ResponseBody byte[] getToscaGraph(@PathVariable String id) {
         log.info("REST request to get graph of tosca id : {}", id);
         return ts.getToscaGraph(id);
+    }
+
+    @RequestMapping(value = "/toscaEntity/{id}",
+            method = RequestMethod.GET)
+    @ResponseBody String getEntity(@PathVariable String id) {
+        log.info("REST request to get entity for dyn form of tosca id : {}", id);
+        JSONObject entity = new JSONObject();
+        try {
+			entity.put("name", "Sam");
+	        entity.put("country", 2);
+	        entity.put("sla", 0);
+	        entity.put("licenceAgreement", true);
+	        entity.put("description", "I like TOSCA");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return entity.toString();
+    }
+
+    @RequestMapping(value = "/toscaCustomization/{id}",
+            method = RequestMethod.GET)
+    @ResponseBody String getCustomization(@PathVariable String id) {
+        log.info("REST request to get customization of dyn form of tosca id : {}", id);
+        JSONArray customization = null;
+        try {
+        	customization = new JSONArray("[{\"name\": \"name\",	\"title\": \"Name\",	\"required\": true,	\"type\": {	\"view\": \"input\"	}},{\"name\": \"sla\",\"title\": \"SLA\",\"type\": {\"view\": \"select\",\"options\": [{\"id\": 0,\"name\": \"BigCity\"},{\"id\": 1,\"name\": \"SmallCity\"}]}},{\"name\": \"country\",\"title\": \"Country\",\"type\": {\"view\": \"select\",\"options\": [{\"id\": 0,\"name\": \"USA\"},{\"id\": 1,\"name\": \"German\"},{\"id\": 2,\"name\": \"Russia\"}]},{\"name\": \"licenceAgreement\",\"title\": \"LicenceAgreement\",\"type\": {\"view\": \"checkbox\"}},{\"name\": \"description\",\"title\": \"Description\",\"type\": {\"view\": \"textarea\"}}]");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return customization.toString();
     }
 }
