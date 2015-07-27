5d8695136aceda3da4e9edd0f0e2dff194811dfc
Luca Gioppo <gioppoluca@libero.it>
2015-07-27 14:54:44 +0200

    added dummy return data

--- rest-component/src/main/java/eu/cloudopting/web/rest/CustomizationController.java
+++ rest-component/src/main/java/eu/cloudopting/web/rest/CustomizationController.java
@@ -22,7 +22,36 @@ public class CustomizationController {
 	@RequestMapping(value = "/application/{idApp}/getCustomizationForm",
             method = RequestMethod.GET)
 	public void getCustomizationForm(@PathVariable("appId") final Long id){
-		
+		/*
+		{
+    "type": "object",
+    "title": "Compute",
+    "properties": {
+      "node_id":  {
+        "title": "Node ID",
+        "type": "string"
+      },
+      "node_label":  {
+        "title": "Node Label",
+        "type": "string",
+        "pattern": "^\\S+@\\S+$",
+        "description": "Email will be used for evil."
+      },
+      "memory":  {
+        "title": "Memory",
+        "type": "string",
+        "enum": ["512","1024","2048"]
+      },
+      "cpu": {
+        "title": "CPU",
+        "type": "integer",
+        "maxLength": 20,
+        "validationMessage": "Don't be greedy!"
+      }
+    },
+    "required": ["node_id","node_label","memory", "cpu"]
+  }
+  */
 	}
 
 }
