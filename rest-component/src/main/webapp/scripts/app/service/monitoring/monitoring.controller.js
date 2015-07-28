b8577780880204dd9da4b9d0611707a24a455ef6
Xavier Cases <xavier.cases.external@atos.net>
2015-07-28 15:12:07 +0200

    Changed the graph to a more proper aproach

--- rest-component/src/main/webapp/scripts/app/service/monitoring/monitoring.controller.js
+++ rest-component/src/main/webapp/scripts/app/service/monitoring/monitoring.controller.js
@@ -13,19 +13,20 @@ angular.module('cloudoptingApp')
             // Chart data records -- each entry in this array corresponds to a point on
             // the chart.
             data: [
-                { year: '2008', value: 20 },
-                { year: '2009', value: 10 },
-                { year: '2010', value: 5 },
-                { year: '2011', value: 5 },
-                { year: '2012', value: 20 }
+                { time: '2001', disk: 20, cpu: 12, ram: 15 },
+                { time: '2002', disk: 10, cpu: 5, ram: 13 },
+                { time: '2003', disk: 5, cpu: 20, ram: 19 },
+                { time: '2004', disk: 5, cpu: 3, ram: 0 },
+                { time: '2005', disk: 20, cpu: 10, ram: 5 }
             ],
             // The name of the data record attribute that contains x-values.
-            xkey: 'year',
+            xkey: 'time',
             // A list of names of data record attributes that contain y-values.
-            ykeys: ['value'],
+            ykeys: ['disk', 'cpu', 'ram'],
             // Labels for the ykeys -- will be displayed when you hover over the
             // chart.
-            labels: ['Value']
+            labels: ['Disk', 'CPU', 'RAM'],
+            lineColors: ['green', 'blue', 'orange']
         });
     }
 );
