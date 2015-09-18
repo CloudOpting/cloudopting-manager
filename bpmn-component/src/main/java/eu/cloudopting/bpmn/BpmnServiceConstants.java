a58130e16790905b8eeff6ff8894d36c29e2e3af
Guido Spadotto <guido.spadotto@profesia.it>
2015-09-18 17:59:48 +0200

    Some enumeration values

---
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/BpmnServiceConstants.java
@@ -0,0 +1,38 @@
+package eu.cloudopting.bpmn;
+
+/**
+ * Constants from service.sonstants.js
+ * @author guido
+ *
+ */
+public enum BpmnServiceConstants {
+	SERVICE_SEPARATOR("/"), 
+	SERVICE_ROLE_PUBLISHER("ROLE_PUBLISHER"),
+	SERVICE_ROLE_ADMIN("ROLE_ADMIN"),
+	SERVICE_ROLE_OPERATOR("ROLE_OPERATOR"),
+	SERVICE_ROLE_SUBSCRIBER("ROLE_SUBSCRIBER"),
+	SERVICE_STATUS_UNFINISHED("UNFINISHED"),
+	SERVICE_STATUS_UPLOADED("UPLOADED"),
+	SERVICE_STATUS_DRAFT("DRAFT"),
+	SERVICE_STATUS_READY_TO_PUBLISH("READY_TO_PUBLISH"),
+	SERVICE_STATUS_PUBLISHED("PUBLISHED"),
+	SERVICE_STORAGE_CURRENT_APP("currentApplication"),
+	SERVICE_STORAGE_CURRENT_INSTANCE_ID("currentInstanceId"),
+	SERVICE_STORAGE_ACTIVITI("activiti"),
+	SERVICE_FILE_TYPE_PROMO_IMAGE("promoimage"),
+	SERVICE_FILE_TYPE_CONTENT_LIBRARY("contentlibrary"),
+	SERVICE_FILE_TYPE_TOSCA_ARCHIVE("toscaarchive");
+	
+	private final String constantValue;
+	
+	@Override
+    public String toString() {
+        return constantValue;
+    }
+
+	private BpmnServiceConstants(String constantValue) {
+		this.constantValue = constantValue;
+	}
+	
+	
+}
