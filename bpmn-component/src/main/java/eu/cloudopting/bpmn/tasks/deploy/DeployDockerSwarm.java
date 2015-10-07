8df601d8ceb10de891c2957384168474b7e65545
gioppo <gioppo@Development.cs6cloud.internal>
2015-10-07 14:14:37 +0200

    Added swarm integration

--- bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployDockerSwarm.java
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployDockerSwarm.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import eu.cloudopting.docker.DockerService;
 import eu.cloudopting.tosca.ToscaService;
 
 @Service
@@ -14,12 +15,19 @@ public class DeployDockerSwarm implements JavaDelegate {
 	private final Logger log = LoggerFactory.getLogger(DeployDockerSwarm.class);
 	@Autowired
 	ToscaService toscaService;
+	
+	@Autowired
+	DockerService dockerService;
+
 
 	@Override
-	public void execute(DelegateExecution arg0) throws Exception {
+	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
+		String ip = (String) execution.getVariable("ip");
+
 		log.debug("in DeployDockerSwarm");
 //		toscaService.getNodeType("");
+		dockerService.addMachine(ip, 2376);
 		
 	}
 
