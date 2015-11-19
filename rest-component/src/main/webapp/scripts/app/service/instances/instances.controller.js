572722b201e11b38c9147e3af9a0b3d8da057f53
Xavier Cases <xavier.cases.external@atos.net>
2015-11-19 11:41:42 +0100

    Show instance list. Fixing issue #56.

--- rest-component/src/main/webapp/scripts/app/service/instances/instances.controller.js
+++ rest-component/src/main/webapp/scripts/app/service/instances/instances.controller.js
@@ -15,7 +15,22 @@ angular.module('cloudoptingApp')
             //get only the instances of the current application.
             $scope.currentApp = localStorageService.get(SERVICE.STORAGE.CURRENT_APP);
 
-            $scope.instancesList = $scope.currentApp.instancesList;
+            $scope.instancesList = $scope.currentApp.customizationss;
+
+            angular.forEach($scope.instancesList, function(instance, key) {
+                instance.applicationName = $scope.currentApp.applicationName;
+            });
+
+/*
+            angular.forEach(instancesList, function(instancesList, key) {
+                $scope.instancesList.push({
+                    "service_name": $scope.currentApp.applicationName,
+                    "author": customization.customerOrganizationId,
+                    "date": customization.customizationCreation,
+                    "status": customization.statusId
+
+                })
+            })*/
         }
 
 
@@ -23,20 +38,20 @@ angular.module('cloudoptingApp')
         //TODO: Implement button "Search Service" functionality.
 
         //TODO: Implement button go for each instance.
-        $scope.deploy = function() {
-            InstanceService.deploy();
+        $scope.deploy = function(customizationId) {
+            InstanceService.deploy(customizationId);
         };
-        $scope.stop = function() {
-            InstanceService.stop();
+        $scope.stop = function(customizationId) {
+            InstanceService.stop(customizationId);
         };
-        $scope.delete = function() {
-            InstanceService.delete();
+        $scope.delete = function(customizationId) {
+            InstanceService.delete(customizationId);
         };
-        $scope.monitor = function() {
+        $scope.monitor = function(customizationId) {
             //$state.go('monitor');
         };
-        $scope.start = function() {
-            InstanceService.start();
+        $scope.start = function(customizationId) {
+            InstanceService.start(customizationId);
         };
         //Checks for showing the buttons.
         $scope.showDeploy = function(str){
