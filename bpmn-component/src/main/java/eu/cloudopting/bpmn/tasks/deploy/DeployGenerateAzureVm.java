fdb7e494262fa02b6b7457e18c6a0be6a348aa33
Luca Gioppo <gioppoluca@libero.it>
2015-11-30 11:36:21 +0100

    Split cloud management now each cloud provider has its own subprocess (apart from cloudstack that keeps the existing flow for sake of simplicity

---
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployGenerateAzureVm.java
@@ -0,0 +1,45 @@
+package eu.cloudopting.bpmn.tasks.deploy;
+
+import java.util.HashMap;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import eu.cloudopting.cloud.CloudService;
+import eu.cloudopting.tosca.ToscaService;
+
+@Service
+public class DeployGenerateAzureVm implements JavaDelegate {
+	private final Logger log = LoggerFactory.getLogger(DeployGenerateAzureVm.class);
+	@Autowired
+	ToscaService toscaService;
+
+	@Autowired
+	CloudService cloudService;
+
+	@Value("${cloud.doDeploy}")
+	private boolean doDeploy;
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		log.debug("in DeployGenerateAzureVm");
+		String customizationId = (String) execution.getVariable("customizationId");
+		Long cloudAccountId = (Long) execution.getVariable("cloudAccountId");
+		HashMap<String, String> data = toscaService.getCloudData(customizationId);
+		
+		String cloudtask = "";
+		if (this.doDeploy) {
+			cloudtask = cloudService.createVM(cloudAccountId, data.get("cpu"), data.get("memory"), data.get("disk"),execution.getProcessInstanceId());
+		}
+
+		execution.setVariable("cloudtask", cloudtask);
+
+	}
+
+}
