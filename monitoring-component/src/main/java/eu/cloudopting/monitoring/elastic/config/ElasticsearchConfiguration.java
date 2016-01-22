835b108d38d79236d451857b4ddcd5f8f0ea4024
Luca Gioppo <gioppoluca@libero.it>
2016-01-22 15:19:54 +0100

    Elasticsearch monitoring start commit

--- monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/config/ElasticsearchConfiguration.java
+++ monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/config/ElasticsearchConfiguration.java
@@ -1,23 +1,23 @@
 package eu.cloudopting.monitoring.elastic.config;
 
 import javax.annotation.Resource;
-/*
+
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
-*/
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-/*
+
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-*/
-//@Configuration
-/*
+
+@Configuration
+
 @EnableElasticsearchRepositories(basePackages = "org/springframework/data/elasticsearch/repositories")
 public class ElasticsearchConfiguration {
 	@Resource
@@ -38,4 +38,3 @@ public class ElasticsearchConfiguration {
 		return new ElasticsearchTemplate(client());
 	}
 }
-*/
