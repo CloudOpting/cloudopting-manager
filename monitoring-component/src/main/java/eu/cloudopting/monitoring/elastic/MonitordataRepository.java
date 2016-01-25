a54a0c2d4cbc769a6d4b7cf1e938bfdb8d53b9e0
Luca Gioppo <gioppoluca@libero.it>
2016-01-25 12:00:26 +0100

    Proper start of value substitution

--- monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/MonitordataRepository.java
+++ monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/MonitordataRepository.java
@@ -11,8 +11,7 @@ import eu.cloudopting.monitoring.elastic.data.Monitordata;
 public interface MonitordataRepository extends ElasticsearchRepository<Monitordata, String> {
 	public Monitordata findOne(String id); 
 	
-//	@Query(value="{\"query\": {\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"path\"],\"query\" : \"/html\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"host\": \"66.249.78.191\"}}]}}}}")
-	@Query(value="{\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"path\"],\"query\" : \"/html\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"host\": \"66.249.78.191\"}}]}}}")
-	public List<Monitordata> findCustom();
+	@Query(value="{\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"path\"],\"query\" : \"/html\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"host\": \"?0\"}}]}}}")
+	public List<Monitordata> findCustom(String container);
 
 }
