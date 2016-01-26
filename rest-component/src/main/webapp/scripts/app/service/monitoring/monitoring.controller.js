55b4fb663507701d8fae14fa8aaf4742d0d0a18a
Xavier Cases <xavier.cases.external@atos.net>
2016-01-26 13:23:24 +0100

    Added charts in monitoring. Issue https://github.com/CloudOpting/cloudopting-manager/issues/150

--- rest-component/src/main/webapp/scripts/app/service/monitoring/monitoring.controller.js
+++ rest-component/src/main/webapp/scripts/app/service/monitoring/monitoring.controller.js
@@ -5,12 +5,13 @@ angular.module('cloudoptingApp')
 
         //We should get the information of the current instance.
 
-        var graph = null;
+        //var graph = null;
 
-        var callback = function(graph) {
+        var callback_line = function(graph) {
+            console.log(graph);
             new Morris.Line({
                 // ID of the element in which to draw the chart.
-                element: 'myfirstchart',
+                element: 'chart_line',
                 // Chart data records -- each entry in this array corresponds to a point on
                 // the chart.
                 data: graph.data,
@@ -32,30 +33,84 @@ angular.module('cloudoptingApp')
                 lineColors: graph.lineColors/*['green', 'blue', 'orange']*/
             });
         };
+        MonitoringService.findObject(1, 1, callback_line);
 
-        MonitoringService.findObject(1, 1, callback);
 
-        var elasticgraph = null;
-        var elasticcallback = function(elasticgraph) {
-            new Morris.Line({
+        //var graph_bar = null;
+
+        var callback_bar = function(graph) {
+            console.log(graph);
+            new Morris.Bar({
                 // ID of the element in which to draw the chart.
-                element: 'elasticchart',
+                element: 'chart_bar',
                 // Chart data records -- each entry in this array corresponds to a point on
                 // the chart.
-                data: elasticgraph.data,
+                data: graph.data,
 
+                /*[
+                 { time: '2001', disk: 20, cpu: 12, ram: 15 },
+                 { time: '2002', disk: 10, cpu: 5, ram: 13 },
+                 { time: '2003', disk: 5, cpu: 20, ram: 19 },
+                 { time: '2004', disk: 5, cpu: 3, ram: 0 },
+                 { time: '2005', disk: 20, cpu: 10, ram: 5 }
+                 ],*/
                 // The name of the data record attribute that contains x-values.
-                xkey: elasticgraph.xkey,/*'time',*/
+                xkey: graph.xkey,/*'time',*/
                 // A list of names of data record attributes that contain y-values.
-                ykeys: elasticgraph.ykeys,/*['disk', 'cpu', 'ram'],*/
+                ykeys: graph.ykeys,/*['disk', 'cpu', 'ram'],*/
                 // Labels for the ykeys -- will be displayed when you hover over the
                 // chart.
-                labels: elasticgraph.labels,/*['Disk', 'CPU', 'RAM'],*/
-                lineColors: elasticgraph.lineColors/*['green', 'blue', 'orange']*/
+                labels: graph.labels,/*['Disk', 'CPU', 'RAM'],*/
+                lineColors: graph.lineColors/*['green', 'blue', 'orange']*/
+            });
+        };
+        MonitoringService.findObject(1, 1, callback_bar);
+
+        //var elasticgraph = null;
+        var elasticcallback_line = function(graph) {
+            console.log(graph);
+            new Morris.Line({
+                // ID of the element in which to draw the chart.
+                element: 'elasticchart_line',
+                // Chart data records -- each entry in this array corresponds to a point on
+                // the chart.
+                data: graph.data,
+
+                // The name of the data record attribute that contains x-values.
+                xkey: graph.xkey,/*'time',*/
+                // A list of names of data record attributes that contain y-values.
+                ykeys: graph.ykeys,/*['disk', 'cpu', 'ram'],*/
+                // Labels for the ykeys -- will be displayed when you hover over the
+                // chart.
+                labels: graph.labels,/*['Disk', 'CPU', 'RAM'],*/
+                lineColors: graph.lineColors/*['green', 'blue', 'orange']*/
             });
         };
         
-        MonitoringService.findOneDataById(1,elasticcallback);
+        MonitoringService.findOneDataById(1,elasticcallback_line);
+
+        //var elasticgraph_B = null;
+        var elasticcallback_bar = function(graph) {
+            console.log(graph);
+            new Morris.Bar({
+                // ID of the element in which to draw the chart.
+                element: 'elasticchart_bar',
+                // Chart data records -- each entry in this array corresponds to a point on
+                // the chart.
+                data: graph.data,
+
+                // The name of the data record attribute that contains x-values.
+                xkey: graph.xkey,/*'time',*/
+                // A list of names of data record attributes that contain y-values.
+                ykeys: graph.ykeys,/*['disk', 'cpu', 'ram'],*/
+                // Labels for the ykeys -- will be displayed when you hover over the
+                // chart.
+                labels: graph.labels,/*['Disk', 'CPU', 'RAM'],*/
+                //lineColors: graph.lineColors/*['green', 'blue', 'orange']*/
+            });
+        };
+
+        MonitoringService.findOneDataById(1,elasticcallback_bar);
 
     }
 );
