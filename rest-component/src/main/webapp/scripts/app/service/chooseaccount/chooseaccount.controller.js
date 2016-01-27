0c04a6e9d7912f4f1a66fc9545326efa5288015c
Xavier Cases <xavier.cases.external@atos.net>
2016-01-27 16:05:54 +0100

    Bug fixing. Closes #162

--- rest-component/src/main/webapp/scripts/app/service/chooseaccount/chooseaccount.controller.js
+++ rest-component/src/main/webapp/scripts/app/service/chooseaccount/chooseaccount.controller.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('cloudoptingApp')
-    .controller('ChooseAccountController', function (SERVICE, $rootScope, $scope, $state, $timeout, localStorageService) {
+    .controller('ChooseAccountController', function (SERVICE, $rootScope, $scope, $state, $timeout, localStorageService, OrganizationService) {
 
         var organization = localStorageService.get(SERVICE.STORAGE.WIZARD_INSTANCES.ORGANIZATION);
         var func = localStorageService.get(SERVICE.STORAGE.WIZARD_INSTANCES.FUNCTION);
 
         //Get account for organization and show it.
-        $scope.cloudAccountsList = organization.cloudAccount;
+
+        OrganizationService.findById(organization.id)
+            .success(function (organization) {
+                $scope.org = organization;
+                $scope.cloudAccountsList = organization.cloudAccountss;
+            });
+
 
         //Selected organization, execute the function with the account selected.
         $scope.choose = function(cloudAccount) {
