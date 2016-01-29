dc5a0271283287a97b96f9d67f2c3b20ab8eb1fb
Ciprian Pavel <ciprian.pavel@teamnet.ro>
2016-01-29 12:35:59 +0200

    #160

--- common-component/src/main/java/eu/cloudopting/service/impl/ApplicationServiceImpl.java
+++ common-component/src/main/java/eu/cloudopting/service/impl/ApplicationServiceImpl.java
@@ -1,12 +1,17 @@
 package eu.cloudopting.service.impl;
 
 import eu.cloudopting.domain.Applications;
+import eu.cloudopting.events.api.SearchCommonUtil;
+import eu.cloudopting.events.api.constants.ClientOperation;
+import eu.cloudopting.events.api.exceptions.ConflictException;
 import eu.cloudopting.repository.ApplicationsRepository;
 import eu.cloudopting.service.ApplicationService;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
@@ -15,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import eu.cloudopting.events.api.service.AbstractService;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * @author Daniel P.
@@ -22,6 +28,8 @@ import javax.inject.Inject;
 @Service
 @Transactional
 public class ApplicationServiceImpl extends AbstractService<Applications> implements ApplicationService {
+
+    private final Logger logger = LoggerFactory.getLogger(ApplicationServiceImpl.class);
 
     @Inject
     private ApplicationsRepository applicationsRepository;
@@ -48,7 +56,15 @@ public class ApplicationServiceImpl extends AbstractService<Applications> implem
 	public Page<Applications> findForApiGetAll(int page, int size, String sortBy, String sortOrder,
 			String filterObj) {
         final Sort sortInfo = constructSort(sortBy, sortOrder);
-        return applicationsRepository.findForApiGetAll(new PageRequest(page, size, sortInfo));
+
+        List<Triple<String, ClientOperation, String>> parsedQuery;
+
+        if (filterObj != null) {
+            return new PageImpl(searchAll(filterObj));
+        }
+        else {
+            return applicationsRepository.findForApiGetAll(new PageRequest(page, size, sortInfo));
+        }
 	}
 
 	@Override
