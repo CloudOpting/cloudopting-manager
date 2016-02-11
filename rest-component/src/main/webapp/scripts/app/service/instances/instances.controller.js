8093a9949f201692ea0e7406b84d182f4afaf5d6
Xavier Cases <xavier.cases.external@atos.net>
2016-02-11 16:52:10 +0100

    Added pagination to the lists. Added some checks on callbacks.

--- rest-component/src/main/webapp/scripts/app/service/instances/instances.controller.js
+++ rest-component/src/main/webapp/scripts/app/service/instances/instances.controller.js
@@ -1,19 +1,28 @@
 'use strict';
 
 angular.module('cloudoptingApp')
-    .controller('InstancesController', function (SERVICE, $scope, $state, $log, $location, Principal, localStorageService, InstanceService, ProcessService, $window, Blob, FileSaver) {
+    .controller('InstancesController', function (SERVICE, $scope, $state, $log, $location, Principal, $filter,
+                                                 localStorageService, InstanceService, ProcessService, $window, Blob, FileSaver) {
 
-        $scope.instancesList = null;
+        $scope.currentPage = 0;
+        $scope.pageSize = 8;
+        $scope.instancesList = [];
+        $scope.searchTextInstance = '';
+        $scope.numberOfPages = function(){
+            return Math.ceil($scope.dataLength()/$scope.pageSize);
+        };
+        $scope.dataLength = function(){
+            return $filter('filter')($scope.instancesList, $scope.searchTextInstance).length;
+        };
 
         if(Principal.isInRole(SERVICE.ROLE.SUBSCRIBER)) {
             //Get all instances of the user if it is a SUBSCRIBER.
             var callback = function(data, status, headers, config){
-                $log.info(data);
+                if(checkStatusCallback(data, status, headers, config, "Instances ok")) {
+                    $scope.instancesList = data;
+                }
             };
-            InstanceService.findAll(callback)
-                .success(function (instances) {
-                    $scope.instancesList = instances;
-                });
+            InstanceService.findAll(callback);
         } else if(Principal.isInRole(SERVICE.ROLE.ADMIN) || Principal.isInRole(SERVICE.ROLE.OPERATOR) ) {
             //If the user is an ADMIN or an OPERATOR and they comes from DETAIL screen
             //get only the instances of the current application.
@@ -39,8 +48,7 @@ angular.module('cloudoptingApp')
 
         $scope.test = function(instance) {
             var callback = function (data, status, headers, config) {
-                checkStatusCallback(data, status, headers, config, "Test requested.");
-                if($scope.errorMessage==null) {
+                if(checkStatusCallback(data, status, headers, config, "Test requested.")) {
                     if(data) {
                         var zip = new Blob([data], {type: 'application/zip'});
                         var fileName = 'TOSCA_Archive_Test.zip';
@@ -53,14 +61,18 @@ angular.module('cloudoptingApp')
 
         $scope.demo = function(instance) {
             var callback = function (data, status, headers, config) {
-                checkStatusCallback(data, status, headers, config, "Demo requested.");
+                if(checkStatusCallback(data, status, headers, config, "Demo requested.")){
+                    //Do something here if all went ok.
+                }
             };
             ProcessService.test(instance, callback);
         };
 
         $scope.deploy = function(instance) {
             var callback = function (data, status, headers, config) {
-                checkStatusCallback(data, status, headers, config, "Deploy requested.");
+                if(checkStatusCallback(data, status, headers, config, "Deploy requested.")){
+                    //Do something here if all went ok.
+                }
             };
             ProcessService.deploy(instance, callback);
         };
@@ -68,14 +80,18 @@ angular.module('cloudoptingApp')
         $scope.stop = function(instance) {
             $window.alert('Not implemented yet');
             var callback = function(data, status, headers, config){
-                $log.info(data);
+                if(checkStatusCallback(data, status, headers, config, "Stop requested.")){
+                    //Do something here if all went ok.
+                }
             };
             //InstanceService.stop(instance);
         };
         $scope.delete = function(instance) {
             $window.alert('Not implemented yet');
             var callback = function(data, status, headers, config){
-                $log.info(data);
+                if(checkStatusCallback(data, status, headers, config, "Delete requested.")){
+                    //Do something here if all went ok.
+                }
             };
             //InstanceService.delete(instance);
         };
@@ -86,7 +102,9 @@ angular.module('cloudoptingApp')
         $scope.start = function(instance) {
             $window.alert('Not implemented yet');
             var callback = function(data, status, headers, config){
-                $log.info(data);
+                if(checkStatusCallback(data, status, headers, config, "Start requested.")){
+                    //Do something here if all went ok.
+                }
             };
             //InstanceService.start(instance);
         };
@@ -122,13 +140,15 @@ angular.module('cloudoptingApp')
                         $state.go('login');
                     }, 3000);
                 }
+                return false;
             }else if(status!=200 && status!=201) {
                 //Show message
                 $scope.errorMessage = "An error occurred. Wait a moment and try again, if problem persists contact the administrator";
-
+                return false;
             } else {
                 //Return to the list
                 $scope.infoMessage = message + " Successfully done!";
+                return true;
             }
         };
     }
