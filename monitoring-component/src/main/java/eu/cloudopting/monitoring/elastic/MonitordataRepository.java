b66253d19edaedb5ee2e424f00fd87e4cbc7db30
Gioppo Luca <gioppoluca@libero.it>
2016-02-18 12:45:34 +0100

    Getting monitoring info for elastic from DB

--- monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/MonitordataRepository.java
+++ monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/MonitordataRepository.java
@@ -13,9 +13,9 @@ import eu.cloudopting.monitoring.elastic.data.Monitordata;
 public interface MonitordataRepository extends ElasticsearchRepository<Monitordata, String> {
 	public Monitordata findOne(String id); 
 	
-	@Query(value="{\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"path\"],\"query\" : \"/html\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"host\": \"?0\"}}]}}}")
+	@Query(value="{\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"path\"],\"query\" : \"/html\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"container_name\": \"?0\"}}]}}}")
 	public List<Monitordata> findCustom(String container, Pageable sort);
 
-	@Query(value="{\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"?2\"],\"query\" : \"?1\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"host\": \"?0\"}}]}}}")
+	@Query(value="{\"filtered\": {\"query\": {\"simple_query_string\" : {\"fields\" : [\"?2\"],\"query\" : \"?1\"}},\"filter\": {\"and\": [{\"range\" : {\"@timestamp\" : {\"gte\": \"2015-01-13T10:55:28+01:00\",\"lte\": \"now\",\"time_zone\": \"+01:00\"}}},{\"term\": {\"container_name\": \"?0\"}}]}}}")
 	public List<Monitordata> findMonitorData(String container, String condition, String fields, Pageable sort);
 }
--- monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/MonitordataService.java
+++ monitoring-component/src/main/java/eu/cloudopting/monitoring/elastic/MonitordataService.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import eu.cloudopting.domain.Customizations;
 import eu.cloudopting.monitoring.elastic.data.Monitordata;
+import eu.cloudopting.service.CustomizationService;
 
 import java.util.List;
 
@@ -13,9 +16,13 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+@Transactional
 @Service
 public class MonitordataService {
+	
+	@Autowired
+	CustomizationService customizationService;
+
 	
 	@Autowired
 	private MonitordataRepository monitordataRepository;
@@ -37,6 +44,18 @@ public class MonitordataService {
 
 		public List<Monitordata> getMonitorData(String container, String condition, String fields, String type, String pagination){
 			return monitordataRepository.findCustom(container, new PageRequest(0,20,new Sort(new Sort.Order(Sort.Direction.ASC,"@timestamp"))));
+		}
+		
+		public void getAllMonitorData(Long customizationId){
+			// recover the customization
+			Customizations cust = customizationService.findOne(customizationId);
+			// get info on elastic form Db
+//			cust.
+			
+			// with info I get data to pass to the get monitor data
+			
+			// will return a list of list
+			
 		}
 
 }
