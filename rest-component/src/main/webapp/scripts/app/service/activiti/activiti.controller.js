ea1b87cce3928896ceaa582e1aef32cc324f604a
Luca Gioppo <gioppoluca@libero.it>
2016-02-25 16:27:29 +0100

    Added part for creating page for showing activiti diagram for deployed process

---
+++ rest-component/src/main/webapp/scripts/app/service/activiti/activiti.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('cloudoptingApp').controller('ActivitiController',
+		function(SERVICE, $scope, $state, $log, ActivitiService) {
+			// need to know if need to do something to set the $scope.processId
+	
+
+		});
