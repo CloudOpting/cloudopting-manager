bc0c631d2bacc2c4437e98e64feca5ed5143c0da
Xavier Cases Camats <xeviscc@gmail.com>
2016-02-29 16:48:44 +0100

    not used import

--- docker-component/src/test/java/eu/cloudopting/dockertests/DockerTests.java
+++ docker-component/src/test/java/eu/cloudopting/dockertests/DockerTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.util.Map;
 
-import org.apache.log4j.spi.LoggerFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
