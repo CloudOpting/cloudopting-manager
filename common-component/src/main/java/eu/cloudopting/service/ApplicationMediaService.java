af44a00dd0628680f62490073dae590a3c37119f
DanJ <dan.jurca@teamnet.ro>
2016-03-30 19:40:35 +0300

    Fix for #256

--- common-component/src/main/java/eu/cloudopting/service/ApplicationMediaService.java
+++ common-component/src/main/java/eu/cloudopting/service/ApplicationMediaService.java
@@ -1,8 +1,17 @@
 package eu.cloudopting.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import eu.cloudopting.domain.ApplicationMedia;
 import eu.cloudopting.events.api.service.BaseService;
 
 public interface ApplicationMediaService extends BaseService<ApplicationMedia> {
 
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN') or (principal.organizationId == #entity.applicationId.organizationId.id)")
+	ApplicationMedia create(ApplicationMedia entity);
+	
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN') or (principal.organizationId == #entity.applicationId.organizationId.id)")
+	void update(ApplicationMedia entity);
 }
