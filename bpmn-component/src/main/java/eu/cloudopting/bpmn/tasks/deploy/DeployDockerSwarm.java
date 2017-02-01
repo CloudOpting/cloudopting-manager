c58bd6ee451862dfea5d02c6dbef6d6fde302374
Luca Gioppo <gioppoluca@libero.it>
2017-02-01 16:11:00 +0100

    Modifiers for proper swarm integration

--- bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployDockerSwarm.java
+++ bpmn-component/src/main/java/eu/cloudopting/bpmn/tasks/deploy/DeployDockerSwarm.java
@@ -24,7 +24,14 @@ public class DeployDockerSwarm implements JavaDelegate {
 	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
 		String ip = (String) execution.getVariable("ip");
+		//TODO al momento è vuoto perchè non lo leggiamo da nulla valutare se leggerlo dinamicamente o da file di configurazione
+		String hostip = (String) execution.getVariable("hostip");
 
+		
+		//TODO per Davide qui va messo il codice che apre al connessione SSH alla macchina apprena creata usando le chiavi che ci sono in sessione ed esegue tramite ssh i comando della stringa che segue
+		
+		String remote_command = "docker run -d swarm join --advertise="+ip+":2375 consul://"+hostip+":8500";
+		
 		log.debug("in DeployDockerSwarm");
 //		toscaService.getNodeType("");
 		dockerService.addMachine(ip, 2376);
